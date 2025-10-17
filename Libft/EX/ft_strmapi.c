392f12d1430c2bc269af4247f6326f8fe6967a5e
Mario Ferreira <marioferreira0212@gmail.com>
2025-10-17 21:56:36 +0100

    ft_strmapi

---
+++ Libft/EX/ft_strmapi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mju-ferr <mju-ferr@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 21:35:08 by mju-ferr          #+#    #+#             */
+/*   Updated: 2025/10/17 21:56:12 by mju-ferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+{
+	unsigned int	i;
+	char	*new;
+
+	new = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/* 
+static char	ft_oddcasex(unsigned int i, char c)
+{
+	if (i % 2 != 0)
+	{
+		if (c >= 'A' && c <= 'Z')
+			c += 32;
+		else if (c >= 'a' && c <= 'z')
+			c -= 32;
+		return (c);
+	}
+	else
+		return (c);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_strmapi("Candid, Tremocos", &ft_oddcasex));
+	return (0);
+} */
